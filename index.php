3368d06737e77c31cda815de845e2df84b8d1ce0
tumi-tumi <kamo.tswene@gmail.com>
2015-05-27 00:17:41 +0200

    updated script

--- index.php
+++ index.php
@@ -3,11 +3,12 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <meta name="viewport" content="width=600px" />
+<!-- <link rel="stylesheet" href="main.css" type="text/css">-->
 <style>
 body {
   text-align: center;
   font-size: 16px;
-  font-family: 'Comic Sans MS', Helvetica, sans-serif;
+  font-family: Papyrus, Helvetica, sans-serif;
 }
 table {
   margin-left:auto; 
@@ -60,29 +61,32 @@ td {
   var board = new Array(9);
 
 function init() {
- 
+  /* use touch events if they're supported, otherwise use mouse events */
   var down = "mousedown"; var up = "mouseup"; 
   if ('createTouch' in document) { down = "touchstart"; up ="touchend"; }
   
-
+  /* add event listeners */
   document.querySelector("input.button").addEventListener(up, newGame, false);
   var squares = document.getElementsByTagName("td");
   for (var s = 0; s < squares.length; s++) {
     squares[s].addEventListener(down, function(evt){squareSelected(evt, getCurrentPlayer());}, false);
   }
   
-
+  /* create the board and set the initial player */
   createBoard();
   setInitialPlayer();
 }
 
 
+/****************************************************************************************/
+/* creating or restoring a game board, adding Xs and Os to the board, saving game state */
+/****************************************************************************************/
 function createBoard() {
 
-
+  /* create a board from the stored version, if a stored version exists */
   if (window.localStorage && localStorage.getItem('tic-tac-toe-board')) {
     
-    
+    /* parse the string that represents our playing board to an array */
     board = (JSON.parse(localStorage.getItem('tic-tac-toe-board')));
     for (var i = 0; i < board.length; i++) {
       if (board[i] != "") {
@@ -90,7 +94,7 @@ function createBoard() {
       }
     }
   }
- 
+  /* otherwise, create a clean board */
   else {  
     for (var i = 0; i < board.length; i++) {
       board[i] = "";                               
@@ -99,15 +103,16 @@ function createBoard() {
   }
 }
 
-
+/*** call this function whenever a square is clicked or tapped ***/
 function squareSelected(evt, currentPlayer) {
   var square = evt.target;
-
+  /* check to see if the square already contains an X or O marker */
   if (square.className.match(/marker/)) {
     alert("Sorry, that space is taken!  Please choose another square.");
     return;
   }
- else {
+  /* if not already marked, mark the square, update the array that tracks our board, check for a winner, and switch players */
+  else {
     fillSquareWithMarker(square, currentPlayer);
     updateBoard(square.id, currentPlayer);
     checkForWinner();
@@ -115,22 +120,35 @@ function squareSelected(evt, currentPlayer) {
   }
 }
 
+/*** create an X or O div and append it to the square ***/
 function fillSquareWithMarker(square, player) {
   var marker = document.createElement('div');
-marker.className = player + "-marker";
+  /* set the class name on the new div to X-marker or O-marker, depending on the current player */
+  marker.className = player + "-marker";
   square.appendChild(marker);
 }
 
+/*** update our array which tracks the state of the board, and write the current state to local storage ***/
 function updateBoard(index, marker) {
   board[index] = marker;
   
- var boardstring = JSON.stringify(board);
+  /* HTML5 localStorage only allows storage of strings - convert our array to a string */
+  var boardstring = JSON.stringify(board);
 
- localStorage.setItem('tic-tac-toe-board', boardstring); 
+  /* store this string to localStorage, along with the last player who marked a square */
+  localStorage.setItem('tic-tac-toe-board', boardstring); 
   localStorage.setItem('last-player', getCurrentPlayer());
 }
 
 
+/***********************************************************************************/
+/* checking for and declaring a winner, after a square has been marked with X or O */
+/***********************************************************************************/
+/* Our Tic Tac Toe board, an array:
+  0 1 2
+  3 4 5
+  6 7 8
+*/
 function declareWinner() {
   if (confirm("We have a winner!  New game?")) {
     newGame();
@@ -145,7 +163,7 @@ function weHaveAWinner(a, b, c) {
   else
     return false;
 }
-
+  /* check rows */
 function checkForWinner() {
 
   var a = 0; var b = 1; var c = 2;
@@ -156,7 +174,7 @@ function checkForWinner() {
     a+=3; b+=3; c+=3;
   }
     
-
+  /* check columns */
   a = 0; b = 3; c = 6;
   while (c < board.length) {
     if (weHaveAWinner(a, b, c)) {
@@ -165,15 +183,16 @@ function checkForWinner() {
     a+=1; b+=1; c+=1;
   }
 
-
+  /* check diagonal right */
   if (weHaveAWinner(0, 4, 8)) {
     return;
   }
-
+  /* check diagonal left */
   if (weHaveAWinner(2, 4, 6)) {
     return;
   }
   
+  /* if there's no winner but the board is full, ask the user if they want to start a new game */
   if (!JSON.stringify(board).match(/,"",/)) {
     if (confirm("It's a draw. New game?")) {
       newGame();
@@ -182,16 +201,22 @@ function checkForWinner() {
 }
 
 
+/****************************************************************************************/
+/* utilities for getting the current player, switching players, and creating a new game */
+/****************************************************************************************/
 function getCurrentPlayer() {
   return document.querySelector(".current-player").id;
 }
+
+/* set the initial player, when starting a whole new game or restoring the game state when the page is revisited */
 function setInitialPlayer() {
   var playerX = document.getElementById("X");
   var playerO = document.getElementById("O");
   playerX.className = "";
   playerO.className = "";
     
- if (!window.localStorage || !localStorage.getItem('last-player')) {
+  /* if there's no localStorage, or no last-player stored in localStorage, always set the first player to X by default */
+  if (!window.localStorage || !localStorage.getItem('last-player')) {
     playerX.className = "current-player";
     return;
   } 
@@ -220,10 +245,12 @@ function switchPlayers() {
 }
 
 function newGame() {  
-
+  /* clear the currently stored game out of local storage */
   localStorage.removeItem('tic-tac-toe-board');
   localStorage.removeItem('last-player');
-    createBoard();
+  
+  /* create a new game */
+  createBoard();
 }
 </script>
 </head>
@@ -231,6 +258,8 @@ function newGame() {
 <body onload="init();">
 <?php
  echo "Mkhabela Phethile 12097561";
+ echo "<br/>";
+ echo "Kamogelo Tswene 12163555";
  echo "<br/>";
  echo "!!Play me!!";
 ?>
